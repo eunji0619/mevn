8e35f7a6af1331eaac2f44560e20cef910bb8450
eunji0619 <4484jjj@naver.com>
2022-12-23 17:49:41 +0900

    221223

---
+++ vue/vue-pr01/src/components/vue_pr02.vue
@@ -0,0 +1,20 @@
+<template>
+    <input type="text">
+    <button v-on:click="status=!status">버튼 입니둥</button>
+    <table v-if = 'status'></table>
+</template>
+
+<script>
+export default {
+    name: 'app',
+    data(){
+        return{
+            status : false,
+            
+        }
+    }
+}
+</script>
+
+<style>
+</style>
